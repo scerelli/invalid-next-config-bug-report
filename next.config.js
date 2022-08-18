1ef6ed0929c8fc5332dd8abd358e183cb73bc12d
Stefano Cerelli <hi@stefanocerelli.com>
2022-08-18 12:53:02 +0200

    add deeps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-}
+/** @type {import('next').NextConfig} */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')
+const { i18n } = require('./next-i18next.config')
 
-module.exports = config
+module.exports = withPlugins(
+  [
+    [withBundleAnalyzer, {
+      enabled: process.env.ANALYZE === 'true'
+    }],
+    [withImages],
+  ],
+  {
+    i18n,
+    reactStrictMode: true,
+    images: {
+      domains: [
+        'cdn.sanity.io',
+      ],
+    },
+    trailingSlash: true,
+    experimental: {
+      esmExternals: false
+    },
+  }
+)
